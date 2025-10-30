dbbd2327d96dfc63c81e5eb51f783c499cdf297e
Satria Tri Ferdiansyah <v.satria.ferdiansyah@adira.co.id>
2025-10-30 13:50:05 +0700

    style(widgets): improve welcome card text hierarchy with larger heading

--- resources/views/vendor/filament-panels/widgets/account-widget.blade.php
+++ resources/views/vendor/filament-panels/widgets/account-widget.blade.php
@@ -11,17 +11,13 @@
         />
 
         <div class="fi-account-widget-main">
-            <h2 class="fi-account-widget-heading">
-                {{ __('filament-panels::widgets/account-widget.welcome', ['app' => config('app.name')]) }}
+            <h2 class="text-2xl font-bold text-gray-900 dark:text-white">
+                Welcome, {{ filament()->getUserName($user) }}
             </h2>
-
-            <p class="fi-account-widget-user-name">
-                {{ filament()->getUserName($user) }}
-            </p>
 
             {{-- Added: show user level under name --}}
             @if (filled($user?->level))
-                <p class="text-sm text-gray-500 dark:text-gray-400">
+                <p class="text-sm text-gray-500 dark:text-gray-400 mt-1">
                     {{ $user->level }}
                 </p>
             @endif
