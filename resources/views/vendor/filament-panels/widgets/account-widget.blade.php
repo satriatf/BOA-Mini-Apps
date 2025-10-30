d88882e5c80ab95e63159e9ef4e59abb6fd3e046
Satria Tri Ferdiansyah <v.satria.ferdiansyah@adira.co.id>
2025-10-30 09:53:40 +0700

    Menambahkan level di card welcome dan hapus sign out

---
+++ resources/views/vendor/filament-panels/widgets/account-widget.blade.php
@@ -0,0 +1,32 @@
+@php
+    $user = filament()->auth()->user();
+@endphp
+
+<x-filament-widgets::widget class="fi-account-widget">
+    <x-filament::section>
+        <x-filament-panels::avatar.user
+            size="lg"
+            :user="$user"
+            loading="lazy"
+        />
+
+        <div class="fi-account-widget-main">
+            <h2 class="fi-account-widget-heading">
+                {{ __('filament-panels::widgets/account-widget.welcome', ['app' => config('app.name')]) }}
+            </h2>
+
+            <p class="fi-account-widget-user-name">
+                {{ filament()->getUserName($user) }}
+            </p>
+
+            {{-- Added: show user level under name --}}
+            @if (filled($user?->level))
+                <p class="text-sm text-gray-500 dark:text-gray-400">
+                    {{ $user->level }}
+                </p>
+            @endif
+        </div>
+
+        {{-- Sign out button removed intentionally --}}
+    </x-filament::section>
+</x-filament-widgets::widget>
