c3a0e4f531ad6e08c180e8e255c4b140c86c1f0c
Satria Tri Ferdiansyah <v.satria.ferdiansyah@adira.co.id>
2025-10-30 11:41:59 +0700

    feat(widgets): add yearly tasks overview widget with year filter for projects and non-projects

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -14,7 +14,7 @@ use Filament\Widgets\AccountWidget;
 use Filament\Actions\Action;
 use Filament\Support\Icons\Heroicon;
 use App\Filament\Resources\Users\UserResource;
-use App\Filament\Widgets\YearlyStatsOverview;
+use App\Filament\Widgets\YearlyTasksOverview;
 use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
 use Illuminate\Cookie\Middleware\EncryptCookies;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
@@ -46,7 +46,7 @@ class AdminPanelProvider extends PanelProvider
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\Filament\Widgets')
             ->widgets([
                 AccountWidget::class,
-                YearlyStatsOverview::class,
+                YearlyTasksOverview::class,
             ])
             // Customize user menu: replace Profile with Change Password (go to Employees edit), keep Sign out
             ->userMenuItems([
